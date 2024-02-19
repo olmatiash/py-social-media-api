6b5b956b6fe7a402084e693db53a0a6350904cc9
Alex Matiash <matiash.jr@gmail.com>
2024-02-19 13:16:26 +0200

    add debug_toolbar url

--- py_social_media_api/urls.py
+++ py_social_media_api/urls.py
@@ -25,6 +25,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
     path("api/user/", include("user.urls", namespace="user")),
     path(
         "api/user/",
