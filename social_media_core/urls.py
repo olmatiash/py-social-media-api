255cbb59f57106dd8a0f26ec1c136dfe9a2377ae
Alex Matiash <matiash.jr@gmail.com>
2024-02-19 11:27:46 +0200

    add urls.py for user, social_media_core, social_media_api apps

---
+++ social_media_core/urls.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+
+from social_media_core.views import HashTagViewSet, LikeViewSet, CommentViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+router.register("hash_tags", HashTagViewSet)
+router.register("likes", LikeViewSet)
+router.register("comments", CommentViewSet)
+router.register("posts", PostViewSet)
+
+urlpatterns = router.urls
+
+app_name = "social_media_core"
